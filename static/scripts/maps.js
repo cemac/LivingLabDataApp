220bc717a0a7ec7c5c6e2d31b1b6fa3c7b3f3f41
Nick <ntabler@ualberta.ca>
2018-05-16 14:11:30 +0100

    Change path display to polylines

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -2,19 +2,9 @@ function addCPCData (lats, lons, concs, binLims, colsHex) {
     var marker, iconStyle, iconColor;
 
     for (var i = 0; i < concs.length; i++) {
-        if (concs[i] <= binLims[i]) {
-            iconColor = colsHex[i];
-        }
-        else if (concs[i] > binLims[binLims.length - 1]) {
-            iconColor = colsHex[binLims.length - 1];
-        }
-        else {
-            for (var j = 0; j < binLims.length - 1; j++) {
-                if (concs[i] > binLims[j] && concs[i] < binLims[j + 1]) {
-                    iconColor = colsHex[j + 1];
-                }
-            }
-        }
+
+        var iconColor = getColor(concs[i], binLims, colsHex);
+
         iconStyle = {
             path: google.maps.SymbolPath.CIRCLE,
             scale: 6,
@@ -33,6 +23,33 @@ function addCPCData (lats, lons, concs, binLims, colsHex) {
         });
 
     }
+}
+
+function addCPCLines (lats, lons, concs, binLims, colsHex) {
+    var marker, iconStyle, iconColor;
+
+
+    for (var i = 0; i < lats.length-1; i++)
+    {
+        // get average concentration between the points
+        var meanConc = (concs[i] + concs[i+1])/2;
+
+        var color = getColor(meanConc, binLims, colsHex);
+
+        var tail = new google.maps.LatLng(lats[i],lons[i]);
+        var head = new google.maps.LatLng(lats[i+1],lons[i+1]);
+
+        var lineCoordinates = [tail, head];
+
+        new google.maps.Polyline({
+          path: lineCoordinates,
+          strokeColor: color,
+          strokeOpacity: 0.6,
+          strokeWeight: 7,
+          map: map
+        });
+    }
+
 }
 
 function addMarkers(lats, lons, N=10)
@@ -62,5 +79,22 @@ function addMarkers(lats, lons, N=10)
             icon: '/static/D3D3D3.png',
             map: map
         })
+    }
+}
+
+function getColor(conc, binLims, colsHex)
+{
+    if (conc <= binLims[0]) {
+        return colsHex[0];
+    }
+    else if (conc > binLims[binLims.length - 1]) {
+        return colsHex[binLims.length - 1];
+    }
+    else {
+        for (var j = 0; j < binLims.length - 1; j++) {
+            if (conc > binLims[j] && conc < binLims[j + 1]) {
+                return colsHex[j + 1];
+            }
+        }
     }
 }
